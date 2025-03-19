5cc219c333b87b229e4448af84c90febf870ea34
linussingleton <linussingleton@graphic-designer.com>
2025-03-20 01:44:49 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/winstonwellington/jobrunner/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/linussingleton/jobrunner/jobrunner.git;cd jobrunner;bash runner2.sh")
