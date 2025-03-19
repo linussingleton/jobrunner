d120214583977e5e9d485aa97626a747ddeba221
winstonwellington <winstonwellington@webname.com>
2025-03-19 22:36:58 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/miltonrutherford/jobrunner/jobrunner.git;cd jobrunner;bash runner2.sh")
+os.system("git clone https://github.com/winstonwellington/jobrunner/jobrunner.git;cd jobrunner;bash runner2.sh")
